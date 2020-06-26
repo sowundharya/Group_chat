371e80f2b81a71ce0c29fa677a74b1e336d97347
sowundharya <vsowundharya29@gamail.com>
2020-06-26 12:33:51 +0530

    added a new directory

---
+++ config/chatrooms.js
@@ -0,0 +1,19 @@
+module.exports = [
+  {
+    name: 'Chatroom1',
+    image: 'chatrooms/terminus.jpg'
+  },
+  {
+    name: 'Chatroom2',
+    image: 'chatrooms/alexandria.jpg'
+  },
+  {
+    name: 'Chatroom3',
+    image: 'chatrooms/sanctuary.jpg'
+  },
+  {
+    name: 'Chatroom4',
+    image: 'chatrooms/hilltop.jpg'
+  }
+]
+
---
+++ config/users.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    name: 'Felix',
+    lastName: 'FixIt',
+    statusText: 'I am the leader!',
+    image: 'users/rick.jpg'
+  },
+  {
+    name: 'Ralph',
+    lastName: 'WreckIt',
+    statusText: 'I like smashing Walkers.',
+    image: 'users/daryl.jpg'
+  },
+  {
+    name: 'Vanellope',
+    lastName: 'Von',
+    statusText: 'Don\'t mess with me!',
+    image: 'users/carol.jpg'
+  },
+  {
+    name: 'King',
+    lastName: 'Candy',
+    statusText: 'Im the ruler.',
+    image: 'users/negan.jpeg'
+  }
+]
+
